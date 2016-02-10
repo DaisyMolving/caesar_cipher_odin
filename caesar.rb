ad52cf80b46e84bc46ad25fe2e4b9d2bbed9bd97
Daisy Molving <daisymolving@gmail.com>
2016-02-10 11:25:26 +0000

    wrote failing test for finding index positions of split string letters from alphabet

--- caesar.rb
+++ caesar.rb
@@ -1,5 +1,14 @@
 class Caesar
 
+  
+  def find_indices
+	 alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+	 letters.each do |letter|
+		indices = alphabet.index(letter)
+		indices
+	 end
+
+  end
   def split(input_string)
 	 letters = input_string.split("").to_a
 	 letters
--- caesar_spec.rb
+++ caesar_spec.rb
@@ -12,5 +12,11 @@ describe Caesar do
 	 result = string.split("goodbye")
 	 expect(result).to eq %w(g o o d b y e)
   end
+
+  it 'should replace the letters in letters_array with index positions from alphabet' do
+	 string = Caesar.new
+	 result = string.split("good").find_indices
+	 expect(result).to eq [6, 14, 14, 3]
+  end
 end
 
