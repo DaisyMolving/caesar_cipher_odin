6c633ea13588e89109fdd50344935ebea926731b
Daisy Molving <daisymolving@gmail.com>
2016-02-10 10:46:34 +0000

    wrote a test for split method with alternative string and it fails

--- caesar_spec.rb
+++ caesar_spec.rb
@@ -1,9 +1,16 @@
 require_relative './caesar'
 describe Caesar do
   
-  it 'should split a string input into letters and return an array' do
+  it 'should split a string input "hello" into letters and return an array' do
 	 	string = Caesar.new
 	 	result = string.split("hello")
 		expect(result).to eq ["h", "e", "l", "l", "o"]
   end
+
+  it 'should split a string input "goodbye" into letters and return an array' do
+	 string = Caesar.new
+	 result = string.split("goodbye")
+	 expect(result).to eq %w(g o o d b y e)
+  end
 end
+
