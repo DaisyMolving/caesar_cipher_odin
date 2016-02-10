c48aa9abbf3238522c69ce9b1c545831fdbdf88b
Daisy Molving <daisymolving@gmail.com>
2016-02-10 12:41:14 +0000

    completed find_indices method to take input_string's split letters array and make new array with index positions of letters in alphabet

--- caesar.rb
+++ caesar.rb
@@ -1,15 +1,17 @@
 class Caesar
 
   
-  def find_indices
+  def find_indices(input_string)
+	 letters = split_input(input_string)
+	 indices = []
 	 alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
 	 letters.each do |letter|
-		indices = alphabet.index(letter)
-		indices
+		indices << alphabet.index(letter)
 	 end
-
+		indices
   end
-  def split(input_string)
+  
+  def split_input(input_string)
 	 letters = input_string.split("").to_a
 	 letters
   end
--- caesar_spec.rb
+++ caesar_spec.rb
@@ -3,19 +3,19 @@ describe Caesar do
   
   it 'should split a string input "hello" into letters and return an array' do
 	 	string = Caesar.new
-	 	result = string.split("hello")
+	 	result = string.split_input("hello")
 		expect(result).to eq ["h", "e", "l", "l", "o"]
   end
 
   it 'should split a string input "goodbye" into letters and return an array' do
 	 string = Caesar.new
-	 result = string.split("goodbye")
+	 result = string.split_input("goodbye")
 	 expect(result).to eq %w(g o o d b y e)
   end
 
   it 'should replace the letters in letters_array with index positions from alphabet' do
 	 string = Caesar.new
-	 result = string.split("good").find_indices
+	 result = string.find_indices("good")
 	 expect(result).to eq [6, 14, 14, 3]
   end
 end
